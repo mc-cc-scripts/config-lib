224343f09a218394e254ccb4d8229e15d4c4213a
Josh <dev@cldv.org>
2024-06-29 23:09:39 +0200

    add comments to example program

--- example/configExample.lua
+++ example/configExample.lua
@@ -2,6 +2,8 @@ print("Example program for config.lua")
 local Config = require "../config"
 local args = {...}
 
+-- The defaults are used whenever a value is not yet set.
+-- Values that are not set as a default cannot be used in the config.
 local defaults = {
     ["tset"] = {
         ["description"] = "tset value desc",
@@ -25,12 +27,18 @@ local defaults = {
     }
 }
 
+-- Always initialise the Config with the default values
+-- You can (optionally) add a suffix as second parameter
+-- which will be used internally to store the values.
 Config:init(defaults)
 
 -- The main program should not run when it is called with config as parameter
 if args[1] == "config" then
+    -- To automatically handle all commands, just pass the args to the command method
+    -- and it will provide all prints etc.
     Config:command(args)
 else
+    -- This is not needed, just an example on how you could print all configurations
     local list = Config:list()
     for i = 1, #list do
         local val = Config:get(list[i])
